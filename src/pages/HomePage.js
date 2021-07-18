740f4bb45b3ca0b7d3e3da2dd380e7d4144abfd7
muzammil413tariq <61079442+muzammil413tariq@users.noreply.github.com>
2021-07-19 00:42:38 +0500

    post-migration setup

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Nav from '../scenes/nav/nav';
 import Header from '../scenes/header/header';
 import Mpartial from '../scenes/mpartial/mpartial';
+import PostMitigration from '../scenes/post-mitigration/postMitigration';
 const HomePage = () => {
     return (
         <div>
             <Nav/>
             <Header/>
             <Mpartial/>
+            <PostMitigration/>
+
         </div>
     );
 };
---
+++ src/scenes/post-mitigration/postMitigration.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css'
+const postMitigration = (props) => (
+    <input type="range" id="slider" min="0" max="100" value="1"  />
+
+    );
+
+export default postMitigration;
