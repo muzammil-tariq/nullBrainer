9d51d4407a25c17f948ed54d63ddd9ceaa52bc30
muzammil413tariq <61079442+muzammil413tariq@users.noreply.github.com>
2021-07-19 00:25:38 +0500

    section 2(intro)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from '../scenes/nav/nav';
 import Header from '../scenes/header/header';
+import Mpartial from '../scenes/mpartial/mpartial';
 const HomePage = () => {
     return (
         <div>
             <Nav/>
             <Header/>
+            <Mpartial/>
         </div>
     );
 };
---
+++ src/scenes/mpartial/mpartial.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+const mpartial = (props) => (
+    <section className="mpartial">
+        <div className="row">
+        <div className="title-box col-md-6 col-sm-12">
+                <h2 className="title">GROUND-TRUTH DATA EVERYONE TRUSTS</h2>
+            </div>
+            <div className=" col-md-6 col-sm-12">
+                <p className="into-box">We have combined the best-of-breed technology platforms with an eye towards relieving you of administrative burden. Matterport 3D scans augmented by TruePlan are used to generate consistent Xactimate sheets that are delivered in accord with Actionable Insights compliance rule sets. Each mpartial is produced with full transparency, unprecedented forensic photography, and infallible geospatial data that collectively result in rapid approvals.</p>
+            </div>
+        </div>
+           
+    </section>
+);
+
+export default mpartial;
+
+
