3619903c185b23f4db1ebf17ef8f13a120f71633
muzammil413tariq <61079442+muzammil413tariq@users.noreply.github.com>
2021-07-19 16:06:40 +0500

    postMigration

--- src/scenes/post-mitigration/postMitigration.js
+++ src/scenes/post-mitigration/postMitigration.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './index.css'
 const postMitigration = (props) => (
-    <input type="range" id="slider" min="0" max="100" value="1"  />
 
-    );
+    <section className="mitigration" >
+        <div className="wrapper">
+            <div className="images">
+                <div className="img-1"></div>
+                <div className="img-2"></div>
+            </div>
+            <div className="slider">
+            <input type="range" min="0" max="100" value="50"/>
+            </div>
+        </div>
+    </section>
+
+);
 
 export default postMitigration;
