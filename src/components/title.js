1d4cd7c7f5fd59a256180b99ad06b8fbcf6efc59
muzammil413tariq <61079442+muzammil413tariq@users.noreply.github.com>
2021-07-19 22:43:45 +0500

    divide into components

---
+++ src/components/title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+function title(props) {
+    return (
+        <h2 className="title-com"> Waterfall Fee Structure</h2>
+    );
+}
+
+export default title;
--- src/scenes/waterFall/waterFall.js
+++ src/scenes/waterFall/waterFall.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Title from '../../components/title';
 import './index.css'
 const WaterFall = (props) => {
 
@@ -7,8 +8,7 @@ const WaterFall = (props) => {
             <div className="container">
 
                 <div className="row">
-                    <h2 className="title-work-waterfall"> Waterfall Fee Structure</h2>
-
+                    <Title />
                     <div className="mt-5 pt-5 mb-5">
                         <div className="row">
                             <div className="col-lg-1"></div>
@@ -18,51 +18,51 @@ const WaterFall = (props) => {
                                         <div>Trial Plan</div>
                                     </div>
                                     <div className="body">
-                                       <h4>6.34%</h4>
-                                       <div>
-                                           <div className="d-flex">
-                                               <div>Access to mpartial engine</div>
-                                           </div>
-                                           <div className="d-flex">
-                                               <div>Access to mpartial engine</div>
-                                           </div>
-                                       </div>
-                                       <h4 style={{fontSize:'30px' , marginTop:'101px'}}>Free</h4>
+                                        <h4>6.34%</h4>
+                                        <div>
+                                            <div className="d-flex">
+                                                <div>Access to mpartial engine</div>
+                                            </div>
+                                            <div className="d-flex">
+                                                <div>Access to mpartial engine</div>
+                                            </div>
+                                        </div>
+                                        <h4 style={{ fontSize: '30px', marginTop: '101px' }}>Free</h4>
                                     </div>
                                 </div>
-                            
+
                             </div>
                             <div className="col-lg-5">
-                            <div className="fee-box">
+                                <div className="fee-box">
                                     <div className="header header-paid">
                                         <div>Enterprise Plan</div>
                                     </div>
                                     <div className="body">
-                                       <h4>4.12%</h4>
-                                       <div>
-                                           <div className="d-flex">
-                                               <div>Access to mpartial engine</div>
-                                           </div>
-                                           <div className="d-flex">
-                                               <div>Discounted fee schedule</div>
-                                           </div>
-                                           <div className="d-flex">
-                                               <div>Unlimited collaborators</div>
-                                           </div>
-                                           <div className="d-flex">
-                                               <div>Dedicated account manager</div>
-                                           </div>
-                                           <div className="d-flex">
-                                               <div>Prioritized turnaround time</div>
-                                           </div>
-                                           <div className="d-flex">
-                                               <div>Minimum $750 initial deposit</div>
-                                           </div>
-                                       </div>
-                                       <h4 style={{fontSize:'30px'}}>Free</h4>
+                                        <h4>4.12%</h4>
+                                        <div>
+                                            <div className="d-flex">
+                                                <div>Access to mpartial engine</div>
+                                            </div>
+                                            <div className="d-flex">
+                                                <div>Discounted fee schedule</div>
+                                            </div>
+                                            <div className="d-flex">
+                                                <div>Unlimited collaborators</div>
+                                            </div>
+                                            <div className="d-flex">
+                                                <div>Dedicated account manager</div>
+                                            </div>
+                                            <div className="d-flex">
+                                                <div>Prioritized turnaround time</div>
+                                            </div>
+                                            <div className="d-flex">
+                                                <div>Minimum $750 initial deposit</div>
+                                            </div>
+                                        </div>
+                                        <h4 style={{ fontSize: '30px' }}>Free</h4>
                                     </div>
                                 </div>
-                            
+
                             </div>
                             <div className="col-lg-1"></div>
                         </div>
