05aef91eae4bd5545c3eb82e8098733685d963d3
muzammil413tariq <61079442+muzammil413tariq@users.noreply.github.com>
2021-07-19 17:24:01 +0500

    how it work section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import Nav from '../scenes/nav/nav';
 import Header from '../scenes/header/header';
 import Mpartial from '../scenes/mpartial/mpartial';
 import PostMitigration from '../scenes/post-mitigration/postMitigration';
+import HowItWork from '../scenes/How-It-Work/howItWork';
 const HomePage = () => {
     return (
         <div>
@@ -10,6 +11,7 @@ const HomePage = () => {
             <Header/>
             <Mpartial/>
             <PostMitigration/>
+            <HowItWork/>
 
         </div>
     );
---
+++ src/scenes/How-It-Work/howItWork.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './index.css'
+const howItWork = (props) => (
+    <section className="how-it-work">
+        <div className="container">
+            <div className="row">
+                <h2 className="title-work"> How It Works
+                </h2>
+                <div className="data-points">
+                    <div className="row">
+                        <div className="col-md-4 col-sm-12">
+                            <div className="data-points-item row">
+                                <div className="data-number data_number_1 col-md-4 col-sm-12">
+                                    1
+                                </div>
+                                <div className="data-text  col-md-8 col-sm-12 ">
+                                    <a href="#">                                    Perform pre-mitigation and post-mitigation scans with a Matterport Pro Series camera.
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-12">
+                            <div className="data-points-item row">
+                                <div className="data-number data_number_1 col-md-4 col-sm-12">
+                                    1
+                                </div>
+                                <div className="data-text  col-md-8 col-sm-12 ">
+                                    <a href="#">                                    Perform pre-mitigation and post-mitigation scans with a Matterport Pro Series camera.
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-12">
+                            <div className="data-points-item row">
+                                <div className="data-number data_number_1 col-md-4 col-sm-12">
+                                    1
+                                </div>
+                                <div className="data-text  col-md-8 col-sm-12 ">
+                                    <a href="#">                                    Perform pre-mitigation and post-mitigation scans with a Matterport Pro Series camera.
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="try-now-btn">
+                    <a href="#">
+                        <button className="btn">Try Today</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+);
+
+export default howItWork;
