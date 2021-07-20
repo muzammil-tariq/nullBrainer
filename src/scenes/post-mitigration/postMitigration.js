047a0e22dbb0955a9418d18be4eeda0b6f4e3a76
muzammil413tariq <61079442+muzammil413tariq@users.noreply.github.com>
2021-07-20 16:41:27 +0500

    comparison slider

--- src/scenes/post-mitigration/postMitigration.js
+++ src/scenes/post-mitigration/postMitigration.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import './index.css'
-const postMitigration = (props) => (
-
-    <section className="mitigration" >
-        <div className="wrapper">
-            <div className="images">
-                <div className="img-1"></div>
-                <div className="img-2"></div>
+import React, { useState } from 'react';
+import './index.css';
+import img from '../../assets/comparison.png';
+const PostMitigration = (props) => {
+    const [range, setRange] = useState(50);
+    const onRangeChange = (e) => {
+        const img=document.querySelector('.img-2');
+        const com=document.querySelector('.wrapper .arrows img');
+        img.style.width=e.target.value + "%"
+        com.style.left=(e.target.value - 7) + "%"
+        setRange(e.target.value);
+    }
+    return (
+        <section className="mitigration" >
+            <div className="wrapper">
+                <div className="images">
+                    <div className="img-1"></div>
+                    <div className="img-2"></div>
+                </div>
+                <div className="slider">
+                    <input type="range" min="0" max="100" value={range} onChange={onRangeChange} />
+                </div>
+                <div className="arrows">
+                    <img src={img}></img>
+                </div>
             </div>
-            <div className="slider">
-                <input type="range" min="0" max="100" value="50" />
+            <div className="try-now-btn">
+                <a href="#">
+                    <button className="btn">Try Today</button>
+                </a>
             </div>
-        </div>
-        <div className="try-now-btn">
-            <a href="#">
-                <button className="btn">Try Today</button>
-            </a>
-        </div>
-    </section>
 
-);
+        </section>
 
-export default postMitigration;
+    );
+}
+
+export default PostMitigration;
